2760e70531704af4f0dcd3218d8c7e37b5353884
alotofuptime <77962905+alotofuptime@users.noreply.github.com>
2024-03-29 16:24:39 -0400

    Complete reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,36 @@
-const reverseString = function() {
+/**
+ * SET PARAM to string
+ * SEQUENCE
+ * IF string is empty
+ *    THEN RETURN string
+ * END IF
+ * SET chars to CALL string.split() with empty string
+ * SET backPtr to length of chars array minus 1
+ * SET midIndex to length of chars array divided by 2
+ * FOR
+ *    INT frontPtr to 0
+ *    COND frontPtr is less than midIndex
+ *    FINAL INCREMENT frontPtr
+ *   SEQUENCE
+ *   SET letter in chars array at frontPtr to letter in chars array at backPtr
+ *   DECREMENT backPtr
+ * END FOR
+ * RETURN CALL chars.join() with empty string
+ */
 
+const reverseString = function (string) {
+  if (string === "") return "";
+
+  const chars = string.split("");
+  const midIndex = chars.length / 2;
+  let backPtr = chars.length - 1;
+
+  for (let frontPtr = 0; frontPtr < midIndex; frontPtr++) {
+    [chars[frontPtr], chars[backPtr]] = [chars[backPtr], chars[frontPtr]];
+    backPtr--;
+  }
+
+  return chars.join("");
 };
 
 // Do not edit below this line
