2d6e505794bb855a4377714e9118f421b560ce51
alotofuptime <77962905+alotofuptime@users.noreply.github.com>
2024-03-29 15:37:00 -0400

    Complete repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,31 @@
-const repeatString = function() {
+/**
+ * SEQUENCE
+ * SET 2 params string and num
+ * SET str to an empty string
+ * IF string is equal to an empty string
+ * THEN RETURN str
+ * END IF
+ * IF num is a negative number
+ * THEN return 'ERROR'
+ * END IF
+ * WHILE num is greater than 0
+ * SET str to str + string
+ * DECREMENT num
+ * END WHILE
+ * RETURN str
+ **/
+const repeatString = function (string, num) {
+  let repeatedStr = "";
 
+  if (string === repeatedStr) return repeatedStr;
+  if (num < 0) return "ERROR";
+
+  while (num > 0) {
+    repeatedStr += string;
+    num--;
+  }
+
+  return repeatedStr;
 };
 
 // Do not edit below this line
