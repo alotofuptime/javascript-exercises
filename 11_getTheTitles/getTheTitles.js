fee0ea422df113281132d1b073eed16a57801364
alotofuptime <77962905+alotofuptime@users.noreply.github.com>
2024-03-31 14:14:07 -0400

    Complete getTheTitles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+/**
+ * @param arr: array of objects each containing book data (title, author, etc.)
+ * @returns array of book titles
+ */
+const getTheTitles = function (arr) {
+  return arr.map((book) => book.title);
 };
 
 // Do not edit below this line
