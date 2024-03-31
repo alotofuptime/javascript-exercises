241b7a29e97f7d40be4773be8c179679dbfba1c9
alotofuptime <77962905+alotofuptime@users.noreply.github.com>
2024-03-31 10:20:04 -0400

    Complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+/**
+ * SEQUENCE
+ *  SET lowerCaseStr to string.toLowerCase()
+ *  SET parsedStr to lowerCaseStr.replaceAll() with regex /[!.,]/g, ''
+ *  SET revStr to CALL lowerCaseStr.split('').reverse().join('')
+ *  RETURN parsedStr === lowerCaseStr
+ */
+const palindromes = function (string) {
+  lowerCaseStr = string.toLowerCase();
+  const parsedStr = lowerCaseStr.replaceAll(/[!.,\s]/g, "");
+  const revStr = parsedStr.split("").reverse().join("");
+  return parsedStr === revStr;
 };
 
 // Do not edit below this line
